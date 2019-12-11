3faf8038e2bff38c17571656b5ef01544b5c42d8
Spence Guo <37296692+SpenceGuo@users.noreply.github.com>
2019-12-11 19:11:12 +0800

    update

--- config.py
+++ config.py
@@ -16,3 +16,11 @@ database = 'iot_monitor'
 
 # user table name
 tbname = 'users'
+
+"""
+CSRF:
+Flask-WTF提供了对所有Form表单免受跨站请求伪造（Cross-Site Request Forgery，CSRF）攻击的技术支持
+通过添加动态token令牌的方式,关于CSRF可以自行在网上搜索相关内容
+"""
+CSRF_ENABLED = True
+SECRET_KEY = 'iot_monitor_secret_key'
--- form_check.py
+++ form_check.py
@@ -1,6 +1,7 @@
 """
 check the form information
 """
+import config
 from flask import Flask, render_template, request
 from controller.db_operation import Check, Select, Insert
 from flask_wtf import FlaskForm
@@ -15,11 +16,11 @@ class ResisterForm(FlaskForm):
     # repeat_pwd = StringField(validators=[Length(max=17, min=6, message='密码长度必须大于3并且小于10'), EqualTo("password")])
     username = StringField('username', validators=[DataRequired(), Length(5, 15), Regexp('^[A-Za-z][A-Za-z0-9]*$', 0, 'name is invaild')], render_kw={"placeholder": "please input your username"})
     password = PasswordField('password', validators=[DataRequired(), Length(5, 25)], render_kw={'placeholder': 'set your password'})
-    repeat_pwd = PasswordField("repeat_pwd", validators=[DataRequired(), Length(5, 25), EqualTo(password)], render_kw={'placeholder:': 'set your password again'})
+    repeat_pwd = PasswordField("repeat_pwd", validators=[DataRequired(), Length(5, 25)], render_kw={'placeholder:': 'set your password again'})
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "12345678"
+app.config["SECRET_KEY"] = config.SECRET_KEY
 
 
 @app.route('/')
@@ -27,17 +28,17 @@ def hello_world():
     return render_template('login/test.html')
 
 
-@app.route('/registeraa', methods=['POST', 'GET'])
-def registeraa():
+@app.route('/register', methods=['POST', 'GET'])
+def register():
     if request.method == 'GET':
         return render_template('templates/failed.html')
     else:
         form = ResisterForm(request.form)
         if form.validate_on_submit():
-            return 'success'
+            return 'success!!!'
         else:
             print(form.errors)
-            return 'fail'
+            return 'fail...'
 
 
 if __name__ == '__main__':
